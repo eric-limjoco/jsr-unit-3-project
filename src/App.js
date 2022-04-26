6d7a54de91fb8630460c5762c42bfd522fe52e78
Eric Limjoco <ericlimjoco@gmail.com>
2022-04-26 12:32:51 -0400

    Add diet option to search

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ function App() {
 
   const [recipes, setRecipes] = useState([])
   const [term, setTerm] = useState('chicken')
+  const [diet, setDiet] = useState('')
 
   const getRecipes = async () => {
     const res = await axios.get(
-      `${BASE_URL}&q=${term}&app_id=${APP_ID}&app_key=${APP_KEY}&diet=high-protein`
+      `${BASE_URL}&q=${term}&app_id=${APP_ID}&app_key=${APP_KEY}${diet.length > 0 ?  `&diet=${diet}` : ''}`
     )
     console.log(res.data)
     setRecipes(res.data.hits.map(r => r.recipe))
@@ -25,6 +26,10 @@ function App() {
     setTerm(event.target.value)
   }
 
+  const handleDietChange = (event) => {
+    setDiet(event.target.value)
+  }
+
   const handleSearch = (event) => {
     event.preventDefault()
     getRecipes()
@@ -36,11 +41,17 @@ function App() {
         <h1>Recipe Finder</h1>
       </header>
       <form onSubmit={handleSearch} className="search">
-        <label>Search for a recipe
-          <input type="text" value={term} onChange={handleTermChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form> 
+        <label>Search for a recipe</label>
+        <input type="text" value={term} onChange={handleTermChange} />
+        <select value={diet} onChange={handleDietChange}>
+            <option value="">All</option>
+            <option value="high-protein">High Protein</option>
+            <option value="high-fiber">High Fiber</option>
+            <option value="low-carb">Low-Carb</option>
+            <option value="balanced">Balanced</option>
+          </select>
+        <input type="submit" value="Search" />
+      </form>
       <RecipeList recipes={recipes} />
     </div>
   );
