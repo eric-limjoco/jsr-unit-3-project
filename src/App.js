60255f65bd4a9530f2dfde0653809daa2c9963f1
Eric Limjoco <ericlimjoco@gmail.com>
2022-04-26 12:11:47 -0400

    Add recipe search

--- src/App.js
+++ src/App.js
@@ -9,25 +9,38 @@ function App() {
   const [recipes, setRecipes] = useState([])
   const [term, setTerm] = useState('chicken')
 
+  const getRecipes = async () => {
+    const res = await axios.get(
+      `${BASE_URL}&q=${term}&app_id=${APP_ID}&app_key=${APP_KEY}&diet=high-protein`
+    )
+    console.log(res.data)
+    setRecipes(res.data.hits.map(r => r.recipe))
+  }
+
   useEffect(() => {
-    const getRecipes = async () => {
-      const res = await axios.get(
-        `${BASE_URL}&q=${term}&app_id=${APP_ID}&app_key=${APP_KEY}&diet=high-protein`
-      )
-      console.log(res.data)
-      setRecipes(res.data.hits.map(r => r.recipe))
-    }
     getRecipes()
-  }, [term])
+  }, [])
+
+  const handleTermChange = (event) => {
+    setTerm(event.target.value)
+  }
+
+  const handleSearch = (event) => {
+    event.preventDefault()
+    getRecipes()
+  }
+
   return (
     <div className="App">
       <header>
         <h1>Recipe Finder</h1>
       </header>
-      <div className="search">
-        <input type="text" defaultValue={term} />
-        <button>Search</button>
-      </div> 
+      <form onSubmit={handleSearch} className="search">
+        <label>Search for a recipe
+          <input type="text" value={term} onChange={handleTermChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form> 
       <RecipeList recipes={recipes} />
     </div>
   );
